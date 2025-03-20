af3ff019abe26153e69b28e3d0c1cf1609cfdf52
KyoungMIn YOO <91euriel@gmail.com>
2025-03-21 01:57:40 +0900

    add:  swich, delete 구현 및 components폴더, List,TodoForm.tsx생성

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useAddTodo } from "@/hooks/mutations";
+import { useAddTodo, useDeleteTodo, useSwichTodo } from "@/hooks/mutations";
 import { useTodos } from "@/hooks/queries";
 import {
   useMutation,
@@ -16,13 +16,12 @@ const TodoListPage = () => {
 
   const [title, setTitle] = useState("");
   const [contents, setContents] = useState("");
-
   const { data: todos, isPending, isError } = useTodos();
-
   // const addMutation = useMutation({
-
   // 구조분해할당 : addTodo는 명시적으로 쓰기위해 이름 붙이기 // obj 객체니까 mutate만 쓸거면 구조분해할당한것
   const { mutate: addTodo } = useAddTodo();
+  const { mutate: switchTodo } = useSwichTodo();
+  const { mutate: deleteTodo } = useDeleteTodo();
 
   if (isPending) {
     return <div>Loading...</div>;
@@ -39,35 +38,52 @@ const TodoListPage = () => {
     addTodo({ title, contents, isDone: false });
   };
 
+  const handleSwitch = ({
+    todoId,
+    isDone,
+  }: {
+    todoId: number;
+    isDone: boolean;
+  }) => {
+    switchTodo({
+      todoId,
+      isDone,
+    });
+  };
+
+  const handleDeleted = (todoId: number) => {
+    deleteTodo(todoId);
+  };
+
   return (
     <div>
       <h1>Next TodoList</h1>
 
+      <form className="flex gap=2" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="제목"
+          className="border boerder-gray-300 p-4 rounded-md mb-2"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="내용"
+          className="border boerder-gray-300 p-4 rounded-md mb-2"
+          value={contents}
+          onChange={(e) => setContents(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="border boerder-gray-300 p-4 rounded-md mb-2"
+        >
+          추가
+        </button>
+      </form>
+
       <div>
         <h3>완료목록</h3>
-
-        <form className="flex gap=2" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="제목"
-            className="border boerder-gray-300 p-4 rounded-md mb-2"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="내용"
-            className="border boerder-gray-300 p-4 rounded-md mb-2"
-            value={contents}
-            onChange={(e) => setContents(e.target.value)}
-          />
-          <button
-            type="submit"
-            className="border boerder-gray-300 p-4 rounded-md mb-2"
-          >
-            추가
-          </button>
-        </form>
 
         {todos
           .filter((todo) => todo.isDone === true)
@@ -80,10 +96,24 @@ const TodoListPage = () => {
                 <h2>{todo.title}</h2>
                 <p>{todo.contents}</p>
                 <div className="flex gap-2">
-                  <button className="bg-blue-500 text-white px-2 py-1 rounded-md ">
-                    {todo.isDone ? "취소" : "완료"}
+                  <button
+                    className="bg-blue-500 text-white px-2 py-1 rounded-md "
+                    onClick={() => {
+                      handleSwitch({
+                        todoId: todo.id,
+                        isDone: !todo.isDone,
+                      });
+                    }}
+                  >
+                    {/* {todo.isDone ? "취소" : "완료"} */}
+                    취소
                   </button>
-                  <button className="bg-blue-500 text-white px-2 py-1 rounded-md ">
+                  <button
+                    className="bg-blue-500 text-white px-2 py-1 rounded-md "
+                    onClick={() => {
+                      handleDeleted(todo.id);
+                    }}
+                  >
                     삭제
                   </button>
                 </div>
@@ -104,10 +134,23 @@ const TodoListPage = () => {
                 <h2>{todo.title}</h2>
                 <p>{todo.contents}</p>
                 <div className="flex gap-2">
-                  <button className="bg-blue-500 text-white px-2 py-1 rounded-md ">
-                    {todo.isDone ? "취소" : "완료"}
+                  <button
+                    className="bg-blue-500 text-white px-2 py-1 rounded-md "
+                    onClick={() => {
+                      handleSwitch({
+                        todoId: todo.id,
+                        isDone: !todo.isDone,
+                      });
+                    }}
+                  >
+                    완료
                   </button>
-                  <button className="bg-blue-500 text-white px-2 py-1 rounded-md ">
+                  <button
+                    className="bg-red-500 text-white px-2 py-1 rounded-md "
+                    onClick={() => {
+                      handleDeleted(todo.id);
+                    }}
+                  >
                     삭제
                   </button>
                 </div>
--- src/hooks/mutations.tsx
+++ src/hooks/mutations.tsx
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
+// update
 export const useAddTodo = () => {
   // 새롭게 queryClient만드는게 아니고 프로젝트내 떠돌고 있는 쿼클 사용하겠다는것
   const queryClient = useQueryClient();
@@ -11,11 +12,52 @@ export const useAddTodo = () => {
     }) => {
       const res = await fetch("http://localhost:4000/todos", {
         method: "POST",
-        body: JSON.stringify(newTodo),
+        body: JSON.stringify({ newTodo }),
       });
       return res.json();
     },
     // input값에 제목, 타이틀 입력하면 바로 화면에 안뜬다 // 서버가 동기화가 안되어서 // 동기화시켜주는 코드
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+  });
+};
+
+//
+export const useSwichTodo = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async ({
+      todoId,
+      isDone,
+    }: {
+      todoId: number;
+      isDone: boolean;
+    }) => {
+      const res = await fetch(`http://localhost:4000/todos/${todoId}`, {
+        method: "PATCH",
+        body: JSON.stringify({ isDone }),
+      });
+      return res.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+  });
+};
+
+// 삭제
+export const useDeleteTodo = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (todoId: number) => {
+      const res = await fetch(`http://localhost:4000/todos/${todoId}`, {
+        method: "DELETE",
+      });
+      return res.json();
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["todos"] });
     },
